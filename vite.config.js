31afc2c50c9bb2f435c57db034441dc04ee51c93
ASHUTOSH YADAV <astronaut11010@gmail.com>
2025-11-07 12:20:02 +0530

    second init

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<repo-name>/', // set to your new repo name
+  base: '/School_F-B/',
   server: {
     proxy: {
       '/api': {
